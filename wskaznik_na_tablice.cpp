bd69fc62e037e253480f83371ef0a36f6ae35523
codestormm <emiliaplachta@interia.pl>
2016-08-01 12:35:02 +0200

    Update wskaznik_na_tablice.cpp

--- wskaznik_na_tablice.cpp
+++ wskaznik_na_tablice.cpp
@@ -16,7 +16,9 @@ int tab[ile];
 	cin>>liczba;
 	tab[i]=liczba;
 	}
-//chcemy poruszać się po zdefiniowanej tablicy elementów typu int za pomocą wskaźnika, wiec przypisujemy mu adres pierwszej komórki tablicy
+//chcemy poruszać się po zdefiniowanej tablicy elementów typu int za pomocą wskaźnika, możemy zauważyć pewna analogie pomiedzy wskażnikami, a tablicami (po tablicy poruszamy sie po jej komórkach za pomoca numerów wskaźniki poruszają sie podobnie tyle,że mają dostęp do większego zasobu pamięci i operują na adresach poszczegolnych komórek pamieci.
+//wykorzystujac tę analogie możemy skorzystać z uproszczonego zapsu przypisania pierwszego adresu zadeklarowanej tablicy do wskaźnika; otóż w języku c nazwa sama nazwa tablicy będzie odnosić sie jako wskaźnik do pierwszergo jej elementu
+
 cout<<"Elementy tablicy:\n";
 	for(int i=0;i<ile;i++)
 	{
@@ -25,8 +27,3 @@ cout<<"Elementy tablicy:\n";
 
 
 
-
-
-
-
-}
